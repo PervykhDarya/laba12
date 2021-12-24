0b0ec1e4292f7f8d8137ab80799dc1e8094cd019
PervykhDarya <90053137+PervykhDarya@users.noreply.github.com>
2021-12-24 16:20:47 +0300

    добавлено идз

---
+++ idz.py
@@ -0,0 +1,25 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+data = set()
+
+
+def generate(n, string='', level=0):
+    if n == 0:
+        return string
+    if n == level:
+        data.add(string)
+        return string
+    line = f"({string})"
+    generate(n, string=line, level=level+2)
+    line_2 = f"{string}()"
+    generate(n, string=line_2, level=level + 2)
+    line_3 = f"(){string}"
+    generate(n, string=line_3, level=level+2)
+    return 1
+
+n = int(input())
+generate(n)
+print(data)
